d3819db4f6cb5a5b678a32bb9a1358dfe9e914d2
KzrDev10 <rinnegan3646@gmail.com>
2026-07-30 22:30:59 +0100

    fix: finalize models.py relationships

--- database/models.py
+++ database/models.py
@@ -1,12 +1,23 @@
+from sqlalchemy.orm import backref
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 
-class User(db.model):
-    __tablename__ == 'users'
+class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.String(20),primary_key=True,nullable=False)
     age = db.Column(db.Integer,nullable=False)
     current_savings = db.Column(db.Float,nullable=False,default=0.0)
     monthly_contribution = db.Column(db.Float,nullable=False,default=0.0)
     risk_tolerance = db.Column(db.String(13),nullable=True)
+
+    goals = db.relationship('SavingsGoal',backref='user',lazy=True)
+
+class SavingsGoal(db.Model):
+    __tablename__ = 'savings_goals'
+    id = db.Column(db.Integer,primary_key=True,nullable=False)
+    goal_name= db.Column(db.String(30),nullable=False)
+    target_amount = db.Column(db.Float,nullable=False)
+    timeline_months = db.Column(db.Integer,nullable=False)
+    user_id = db.Column(db.String(20),db.ForeignKey('users.id'),nullable=False)
